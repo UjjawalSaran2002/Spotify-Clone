90698720877fa0860c00fea4e49903e6c2a85e54
UjjawalSaran2002 <ujjawalsaran@gmail.com>
2024-01-23 23:19:57 +0530

    Song cards functioning

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let currentsong=new Audio();
+let songlist;
+let i=0;
+// let a= getSongs();  
 async function getSongs(){
     let a=await fetch("http://127.0.0.1:3000/songs/");
     let response= await a.text();
@@ -13,6 +17,23 @@ async function getSongs(){
     }
    }
    return songs
+}
+async function playSong(songname){
+    // let a=await getSongs();  
+    // let songlist=await getSongName()
+    // console.log(songlist)
+    for (const key in songlist) {
+    var b=songlist[key].split("artist")
+    let c=b[0].replace(".mp3","")
+    c=c.replaceAll(" ","")
+    songname=songname.replaceAll(" ","")
+       if(songname==c){
+        currentsong.pause();
+        currentSong=Audio(a[key])
+        currentSong.play();
+        playtheme();
+       }
+    }
 }
 async function getSongName(){
     let a=await fetch("http://127.0.0.1:3000/songs/");
@@ -30,30 +51,85 @@ async function getSongName(){
    }
    return songs 
 }
+async function playtheme(){
+    let pp=document.getElementsByClassName("playpause")[0].children[0];
+    pp.src="/svgs/pause.svg";
+
+}
+async function pausedtheme(){
+    let pp=document.getElementsByClassName("playpause")[0].children[0];
+    pp.src="/svgs/playbtn.svg"
+
+}
+async function nSong(){
+    i=(Math.abs(i+1))%((a.length));
+    currentsong.pause()
+    currentsong=new Audio(a[i])
+    currentsong.play();
+    console.log("playing next song")
+    playtheme();
+    
+}
+async function pSong(){
+    if(i==0){
+        i=a.length-1;
+    }else{
+
+        i=(i-1);
+    }
+    currentsong.pause()
+    currentsong=new Audio(a[i])
+    currentsong.play();
+    console.log("playing previous song")
+    playtheme();
+
+}
 async function main(){
-    let a=await getSongs();  
-    let songNames= await getSongName();
-    console.log(a); 
-    let i=0;
-    var currentsong= new Audio(a[i]);
+    document.title="Spotify-Clone"
+    songlist=await getSongName();
+    a=await getSongs();
+    let songNames= songlist;
+    // console.log(a); 
+     i=0;
+    currentsong= new Audio(a[i]);
     let playpause=document.getElementsByClassName("playpause")[0]
-    let pp=document.getElementsByClassName("playpause")[0].children[0]
+    // let pp=document.getElementsByClassName("playpause")[0].children[0]
     let prevsong=document.querySelector(".prevsong").children[0]
+    let nextsong=document.querySelector(".nextsong").children[0]
     prevsong.addEventListener("click",()=>{
-        i=(Math.abs(i-1))%((a.length)+1);
-        currentsong.pause()
-        currentsong=new Audio(a[i])
-        currentsong.play();
-        console.log("clicked prev song")
+        // if(i==0){
+        //     i=a.length-1;
+        // }else{
+
+        //     i=(i-1);
+        // }
+        // currentsong.pause()
+        // currentsong=new Audio(a[i])
+        // currentsong.play();
+        // console.log("playing previous song")
+        pSong();
+
+    })
+    nextsong.addEventListener("click",()=>{
+        // i=(Math.abs(i+1))%((a.length));
+        // currentsong.pause()
+        // currentsong=new Audio(a[i])
+        // currentsong.play();
+        // console.log("clicked next song")
+        // playtheme();
+        nSong();
     })
 
     playpause.addEventListener("click",()=>{
         if(currentsong.paused){
+            currentsong.pause()
             currentsong.play();
-            pp.src="/svgs/pause.svg"
+            // pp.src="/svgs/pause.svg"
+            playtheme()
         }else{
             currentsong.pause();
-            pp.src="/svgs/playbtn.svg"
+            // pp.src="/svgs/playbtn.svg"
+            pausedtheme();
         } 
     });
     currentsong.addEventListener("ontimeupdate",()=>{
@@ -62,14 +138,43 @@ async function main(){
     
     })
     // let playpause=document.getElementsByClassName("playpause")[0]
-    let snglst= document.querySelector(".songlist").children[0];
+    let snglst= document.querySelector(".songListCard");
 
-    for (const name of songNames) {
+    for (const name of songNames) {//Making a list of songs with artist names
         let nameart=name.split("artist-")
-        nameart[1]=nameart[1].replace(".mp3","")
-        snglst.innerHTML=snglst.innerHTML+`<li>${name}</li>`
-        console.log(nameart)
+        if(nameart.length>1){
+        nameart[1]=nameart[1].replace(".mp3","")}
+        else{
+            nameart[0]=nameart[0].replace(".mp3","")
+        }
+        if(nameart[1]==undefined){
+            nameart[1]="";
+        }
+        snglst.innerHTML=snglst.innerHTML+` <li class="songListCardli">
+        
+        <img src="/svgs/music.svg" alt="music.svg">
+        <div class="nameartist">
+        <p >${nameart[0]}</p>
+        <p>${nameart[1]}</p>
+        </div>
+        <div class="playnow">playnow <img src="/svgs/playbtn.svg" alt="playbtn">
+        </div>
+        
+        </li>`
+    }
+
+    let songListArr=Array.from(document.getElementsByClassName("songListCardli"))
+    songListArr.forEach(e=>{
+            e.children[2].addEventListener("click",()=>{
+            // console.log(e.children[1].children[0])
+            let songname=(e.children[1].children[0].innerText)
+           playSong(songname)
+        })
+
+    
+    })
+
     }
     
-}
+
 main()   
